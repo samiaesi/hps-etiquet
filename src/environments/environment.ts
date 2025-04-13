12317667ce78b77da564b43d59186098393b1a69
samiaesi <52220112+samiaesi@users.noreply.github.com>
2025-04-13 11:52:42 +0100

    initial commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,15 @@
+
+
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyA1OoafJ-wcUOPzfQyJ7523Fj1gYb6NDdo",
+    authDomain: "dev-hps.firebaseapp.com",
+    databaseURL: "https://dev-hps-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "dev-hps",
+    storageBucket: "dev-hps.appspot.com", // ⚠️ corriger ici "appspot.com" et non "firebasestorage.app"
+    messagingSenderId: "490717937176",
+    appId: "1:490717937176:web:fb151860c152cd3d5d4d6d",
+    measurementId: "G-GY95Q9T34W"
+  }
+};
